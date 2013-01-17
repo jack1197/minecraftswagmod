fcf2f6d07e6f051db27f088e1fcc93ae1da94f63
Jack Wilkie <im-not-tiger@hotmail.com>
2013-01-17 14:37:30 +1300

    fixed customfood

--- jack_1197/swag/common/CustomFood.java
+++ jack_1197/swag/common/CustomFood.java
@@ -14,17 +14,17 @@ public class CustomFood extends ItemFood {
 		super(par1, par2, par3);
 	}
 
-    public Item setHasNetherFX(boolean par1)
-    {
-        this.hasNetherFX = par1;
-        return this;
-    }
+	public Item setHasNetherFX(boolean par1) {
+		this.hasNetherFX = par1;
+		return this;
+	}
 
 	@Override
 	public ItemStack onFoodEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
-		if (hasNetherFX) {
-			((EntityClientPlayerMP) par3EntityPlayer).timeInPortal = 10.0F;
-			((EntityClientPlayerMP) par3EntityPlayer).timeUntilPortal = 10;
+		if (par3EntityPlayer instanceof EntityClientPlayerMP) {
+			if (hasNetherFX) {
+				((EntityClientPlayerMP) par3EntityPlayer).timeInPortal = 10.0F;
+			}
 		}
 		return super.onFoodEaten(par1ItemStack, par2World, par3EntityPlayer);
 	}
